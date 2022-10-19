19403ed3b225846fe6631e509655b2c886d94bd6
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2022-10-19 13:09:12 +0200

    bjvi

--- SavingGrejs/Goose.cs
+++ SavingGrejs/Goose.cs
@@ -3,6 +3,6 @@ using System;
 public class Goose
 {
     public string Name {get; set;}
-    public int Age {get; set;}
-    public type Property { get; set; }
+    public int Age; //{get; set;}
+    public string food {get; set;}
 }
--- SavingGrejs/Program.cs
+++ SavingGrejs/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Goose g = new();
+
+Console.WriteLine("Write your goose's name.");
+g.Name = Console.ReadLine();
+Console.WriteLine($"Your goose's name is now {g.Name}");
+
+Console.WriteLine("Write your goose's age.");
+string tal = Console.ReadLine();
+bool lyckad = int.TryParse(tal, out g.Age);
+Console.WriteLine($"Your goose's age is now {g.Age}");
+
+
+
+Console.ReadLine();
